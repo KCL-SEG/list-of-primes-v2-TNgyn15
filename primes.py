270ed4a86bf9fbac85fd3df86eb38f0a567b0102
Thang Nguyen <thang.1.nguyen@kcl.ac.uk>
2022-10-14 12:30:45 +0100

    Add Solution

--- primes.py
+++ primes.py
@@ -1,6 +1,20 @@
-"""List of prime numbers generator."""
-"""ENTER YOUR SOLUTION HERE!"""
+"List of prime numbers generator."
+
+def isPrime(num):
+    prime = True
+    for i in range(2, num):
+        if (num % i) == 0:
+            prime = False
+            break
+    return prime
 
 def primes(number_of_primes):
+    if number_of_primes < 1:
+        raise ValueError("Invalid input please enter a number greater than 0")
     list = []
+    i = 2
+    while len(list) < number_of_primes:
+        if isPrime(i):
+            list.append(i)
+        i += 1
     return list
